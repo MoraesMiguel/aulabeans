ad70c309c312e3ff1e42df08068e9cde03d62f16
MoraesMiguel <miguelmoraes49@gmail.com>
2021-10-29 17:14:06 -0300

    first comit

---
+++ src/main/java/br/com/projetinho/aulabeans/AulabeansApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetinho.aulabeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulabeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulabeansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projetinho/aulabeans/AulabeansApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetinho.aulabeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulabeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
